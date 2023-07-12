9ddf8e1179ca7a2bda0a08bbbc0c7d73828e4e2c
Praveen singh mahra <Praveen.mehra65@gmail.com>
2023-07-12 13:48:19 +0530

    add otp option and reset option of forget password page

--- js/forgetPassword.js
+++ js/forgetPassword.js
@@ -18,4 +18,47 @@ forgetPasswordForm.addEventListener("submit", function (event) {
 
 function showMessage(message) {
   messageElement.textContent = message;
+  otpForm.classList.remove("hidden");
+  forgetPasswordForm.classList.add("hidden");
 }
+
+const otpForm = document.getElementById("otpForm");
+const resetPasswordForm = document.getElementById("resetPasswordForm");
+const otpInput = document.getElementById("otp");
+const newPasswordInput = document.getElementById("newPassword");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const resetPasswordMessage = document.getElementById("resetPasswordMessage");
+
+otpForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const otp = otpInput.value;
+
+  // Perform OTP verification here
+
+  otpForm.classList.add("hidden");
+  resetPasswordForm.classList.remove("hidden");
+});
+
+resetPasswordForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const newPassword = newPasswordInput.value;
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (newPassword !== confirmPassword) {
+    resetPasswordMessage.textContent =
+      "Passwords do not match. Please try again.";
+    resetPasswordMessage.classList.add("error");
+    return;
+  }
+
+  // Perform the password reset operation here
+
+  newPasswordInput.value = "";
+  confirmPasswordInput.value = "";
+
+  resetPasswordMessage.textContent = "Password reset successful!";
+  resetPasswordMessage.classList.remove("error");
+  window.location.href = "/account.html";
+});
